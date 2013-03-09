14174b068ff34b6975894cbcb2c9ec72ab51fc7f
Andrew Hulsizer <andrew.hulsizer@gmail.com>
2013-03-09 10:07:30 -0600

    Add Xcode Product

---
+++ TravelHack/TravelHack/THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  TravelHack
+//
+//  Created by Andrew Hulsizer on 3/9/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelHack/TravelHack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravelHack
+//
+//  Created by Andrew Hulsizer on 3/9/13.
+//  Copyright (c) 2013 Andrew Hulsizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+    }
+}
