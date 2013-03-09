0380760f805a92655468189625ce3c666f4e23c0
John Lawson <john.lawson@bottlerocketapps.com>
2013-03-09 13:20:40 -0600

    Remove JSONKit. Add THConstants.h. Add LocationSmart.

---
+++ TravelHack/TravelHack/Common/THConstants.h
@@ -0,0 +1,14 @@
+//
+//  THConstants.h
+//  TravelHack
+//
+//  Created by John Lawson on 3/9/13.
+//  Copyright (c) 2013 Team AT&T Travel Hackathon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// LocationSmart Platform API Credentials
+#define LOCATION_SMART_API_KEY		@"93ebfbe0-7b08-11e2-916a-12313d23a402"
+#define LOCATION_SMART_API_PWD		@"5eac22d2"
+#define LOCATION_SMART_CLIENT_ID	@"SXSW-0054"
