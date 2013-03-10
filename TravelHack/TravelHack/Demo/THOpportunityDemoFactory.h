c0d55b3b9abaf6bd93249d25b7cf677cae3bfcc9
Austen Green <austen.green@bottlerocketapps.com>
2013-03-09 23:05:43 -0600

    Added factory for opportunities.

---
+++ TravelHack/TravelHack/Demo/THOpportunityDemoFactory.h
@@ -0,0 +1,15 @@
+//
+//  THOpportunityDemoFactory.h
+//  TravelHack
+//
+//  Created by Austen Green on 3/9/13.
+//  Copyright (c) 2013 Team AT&T Travel Hackathon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface THOpportunityDemoFactory : NSObject
+
++ (NSArray *)opportunities;
+
+@end
--- TravelHack/TravelHack/THAppDelegate.m
+++ TravelHack/TravelHack/THAppDelegate.m
@@ -26,14 +26,6 @@
 	dispatch_async(dispatch_get_main_queue(), ^{
 		self.memberDataSource = [[THMemberDataSource alloc] initWithUsername:AAADVANTAGE_NUMBER password:PASSWORD];
 		
-		[[THAAClient client] loginWithUsername:AAADVANTAGE_NUMBER password:PASSWORD];
-		[[THAAClient client] fetchAccountInformationWithUsername:AAADVANTAGE_NUMBER password:PASSWORD completion:^(id responseData, NSError *error) {
-			NSLog(@"Account Info: %@", error);
-		}];
-		
-		[[THAAClient client] fetchReservationListWithUsername:AAADVANTAGE_NUMBER password:PASSWORD completion:^(id responseData, NSError *error) {
-			NSLog(@"Account Info: %@", error);
-		}];
 	});
 	
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
